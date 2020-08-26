e00267640841416b6444818486d59fccfb76434c
Declan Ordish <declan.ordish@hotmail.co.uk>
2020-08-26 14:31:30 +0100

    asdas

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :requests
-  has_many_attatched :photos
+  has_many_attached :photos
   def self.artist
     User.select { |u| u.artist }
   end
