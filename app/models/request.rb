db50eb197bdaef88122a8cbda805b816036122ad
Declan Ordish <declan.ordish@hotmail.co.uk>
2020-08-26 11:52:31 +0100

    cloudinary made

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,5 @@
 class Request < ApplicationRecord
   belongs_to :quiz
   belongs_to :user
+  has_many_attatched :photos
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :requests
-
+  has_many_attatched :photos
   def self.artist
     User.select { |u| u.artist }
   end
