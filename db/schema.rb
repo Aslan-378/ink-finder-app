6c10af3334e37c14cec9b543e12cbb5ba8cc3da4
Martin Darras <darras.martin@gmail.com>
2020-08-27 16:43:12 +0200

    Added link to artists in nav-bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2020_08_27_134227) do
-=======
-ActiveRecord::Schema.define(version: 2020_08_27_114638) do
->>>>>>> Stashed changes
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
