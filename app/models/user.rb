a6f6423ebe90c6dd0197e7fbfbc9be5bc7584eb1
Martin Darras <darras.martin@gmail.com>
2020-08-27 17:32:27 +0200

    Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   has_many :bookings
   has_many_attached :photos
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change
+
   extend FriendlyId
   friendly_id :instagram, use: :slugged
 
