6c3fd7722c84e3484fbd5994305fb20abc6edcc2
Declan Ordish <declan.ordish@hotmail.co.uk>
2020-08-27 14:43:51 +0100

    client in bookings made

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_120524) do
+ActiveRecord::Schema.define(version: 2020_08_27_134227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2020_08_27_120524) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.bigint "client_id"
+    t.index ["client_id"], name: "index_bookings_on_client_id"
     t.index ["request_id"], name: "index_bookings_on_request_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -72,9 +74,6 @@ ActiveRecord::Schema.define(version: 2020_08_27_120524) do
   end
 
   create_table "requests", force: :cascade do |t|
-
-    t.bigint "quiz_id"
-
     t.string "name"
     t.string "location"
     t.string "body_part"
@@ -133,6 +132,7 @@ ActiveRecord::Schema.define(version: 2020_08_27_120524) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "requests"
   add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "users", column: "client_id"
   add_foreign_key "quizzes", "users"
   add_foreign_key "requests", "quizzes"
   add_foreign_key "requests", "users"
