dcb5e3ea266fd0a67b299e48f0941df68edbc2d8
Martin Darras <darras.martin@gmail.com>
2020-08-28 13:35:03 +0200

    added user creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   include Pundit
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  def configure_permitted_parameters
+    # additional fields on signup
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys:
+                                      [:artist])
+    # additional fields to edit
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys:
+                                      [:artist, :email])
+  end
 
   private
 
