2a0a1f5d8921e8efbe752f1dd3fe8028cc4adf2b
Declan Ordish <declan.ordish@hotmail.co.uk>
2020-09-03 15:53:30 +0100

    changes

--- app/javascript/components/message-scroll.js
+++ app/javascript/components/message-scroll.js
@@ -2,9 +2,8 @@
 
 export const messageScroll = () => {
     console.log("hello")
-    window.addEventListener('load', (event) => {
-        console.log("hi!")
         const messageContainer = document.querySelector('.messages-card-page-container');
-        messageContainer.scrollTop = messageContainer.scrollHeight;
-      });
+        if (messageContainer) {
+            messageContainer.scrollTop = messageContainer.scrollHeight;
+        }
 }
